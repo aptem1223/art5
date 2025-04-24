97b9e6be99322875bb6a1e79561fc6d24e7fdb4e
aptem1223 <sanstop357@gmail.com>
2025-04-24 11:17:18 +0300

    Add files via upload

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+
+        public delegate int SumDelegate(int m, int n);
+
+        class Program
+        {
+            
+            public static int Sum(int m, int n)
+            {
+                int sum = 0;
+                for (int i = m; i <= n; i++)
+                {
+                    sum += i;
+                }
+                return sum;
+            }
+            static void Main(string[] args)
+            {
+                
+                SumDelegate sumDelegate = new SumDelegate(Sum);
+                Console.WriteLine($"Сумма от 1 до 10: {sumDelegate(1, 10)}");
+                Console.WriteLine($"Сумма от 5 до 15: {sumDelegate(5, 15)}");
+                Console.WriteLine($"Сумма от 10 до 20: {sumDelegate(10, 20)}");
+                Console.ReadLine();
+            }
+        }
+    
+
+}
